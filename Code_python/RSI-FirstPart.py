42f6c558228c644e64841e52f64d5a62d1514c52
Matteo NOTTARIS <nottaris.matteo@gmail.com>
2020-11-19 18:34:05 +0100

    update

--- Code_python/RSI-FirstPart.py
+++ Code_python/RSI-FirstPart.py
@@ -8,7 +8,7 @@
 ############################################################################################################################
 
 
-#                   I) First we need to extract the RSI data from Alphavantage 
+#                   I) Declaration of the importations, variables and main function 
 
 #Importation of packages
 import os 
@@ -16,6 +16,7 @@ os.chdir('C:/Users/matno/Desktop/Télecom/2A/MALIS/Project/GIT/MALIS-project/Cod
 import Extraction
 import numpy as np
 import pandas as pd
+import datetime 
 
 
 #My APIKEY for Alphavantage
@@ -32,26 +33,9 @@ my_Path = 'C:/Users/matno/Desktop/Télecom/2A/MALIS/Project/GIT/MALIS-project/Co
 
 parameter1 = 70
 parameter2 = 30
-InitAmount =  1000000
+InitAmount = 1000000
 
 
-
-#This function is extracting all the data from AlphaVantage and updating the corresponding files on the computer
-def updateFiles ():
-    
-    #updating the RSIfile
-    Extraction.getRSI(my_APIKEY, my_interval, my_sj, my_moment, my_time_period, my_Path)
-    
-    #updating the EURUSDfile
-    Extraction.getDualCurrency(my_APIKEY, currency1, currency2, my_Path)
-    
-    return()
-
-############################################################################################################################
-
-
-
-#                    II) Secondly we need to develop the algorithms to generate the set
 
 
 
@@ -65,12 +49,28 @@ def main ():
 
     #creating a panda dictionnary with all data
     DataDico = formatData()
-    
-    #updating the DataDico concidering all data set size
-    new_DataDico = updateData(DataDico)
 
     return()
 
+
+
+
+
+############################################################################################################################
+
+#                       II) We first need to extractthe data from AlphaVantage
+
+
+#This function is extracting all the data from AlphaVantage and updating the corresponding files on the computer
+def updateFiles ():
+    
+    #updating the RSIfile
+    Extraction.getRSI(my_APIKEY, my_interval, my_sj, my_moment, my_time_period, my_Path)
+    
+    #updating the EURUSDfile
+    Extraction.getDualCurrency(my_APIKEY, currency1, currency2, my_Path)
+    
+    return()
 
 
 ###########################################################################################################################
@@ -78,7 +78,114 @@ def main ():
 
 
 
-#                                III) We need to develop tools functions in order to create main
+#                                III) We need to adjust the data to be easily used
+
+    
+    
+#This function formatData() is extracting the data from the excel files and return a dictionnary with the content of the excel sheets
+#dataExcel() allows to read an excel File
+#getFiles () allows to get all excel files in a file
+#intersectionDate (set1, set2) allows to compare two data set and return the common dates
+#
+def formatData():
+    #we initialize commonDate[] with date of day
+    todayDate = datetime.date.today().strftime("%Y-%m-%d")
+    commonDate = ["1900-01-01", todayDate]
+    data = []
+    DicoList = []
+    FilesNames = getFiles(my_Path)
+    for i in range(len(FilesNames)):
+        DataArray = dataExcel(FilesNames[i] + '.xlsx')
+        DataArray = DataArray.to_numpy()
+        data.append(DataArray)
+    #commonDate is a two element list corresponding to the earliest and latest date common to all sets
+    for i in range(len(data) - 1):
+        result = intersectionDate(data[i],data[i+1])
+        if result[0] > commonDate[0]:
+            commonDate[0] = result[0]
+        if result[1] < commonDate[1]:
+            commonDate[1] = result[1]
+    #updating every set with the common dates
+    for i in range(len(data)):
+        data[i] = updateData(data[i], commonDate[0], commonDate[1])
+    #transforming every array in pd.Series
+    for i in range (len(data)):
+        date = data[i][:,0]
+        value = data[i][:,1]
+        TemporaryDic = pd.Series(value)
+        TemporaryDic.index = date
+        DicoList.append(TemporaryDic)
+    #removing some dates inside commonDate using removeSomeDate()
+    for i in range(len(DicoList)-1):
+        result = removeSomeDate(DicoList[i],DicoList[i+1])
+        DicoList[i] = result[0]
+        DicoList[i+1] = result[1]
+    for i in range(len(DicoList)-1,1, -1):
+        print("here")
+        result = removeSomeDate(DicoList[i],DicoList[i-1])
+        DicoList[i] = result[0]
+        DicoList[i-1] = result[1]
+    #creating a main Dictionary containing dictionnary for every set
+    DataDic = pd.Series(DicoList)
+    DataDic.index = FilesNames
+    return(DataDic)
+    
+def dataExcel (FileName):
+    data = pd.read_excel (my_Path + FileName)
+    return(data)
+
+
+def getFiles(path):
+    FilesNames = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.xlsx' in file:
+                FilesNames.append(file.replace('.xlsx','',1))
+    return(FilesNames)
+    
+
+#This function is slicing data array in order to match the intersection date
+def updateData(set, earlydate, latedate):
+    i = 0
+    while set[i][0] != earlydate:
+        i+=1
+    temp_set = set[i:]
+    i = 1
+    while temp_set[len(temp_set)-i][0] != latedate:
+        i+=1
+    set = temp_set [:len(temp_set)-i+1]
+    
+    return (set)
+
+#This function is determining between to data set the earliest and latest common dates 
+def intersectionDate (set1, set2):
+    min1 = np.amin(set1, axis = 0)[0]
+    min2 = np.amin(set2, axis = 0)[0]
+    EarliestDate = max([min1,min2])
+    max1 = np.amax(set1, axis = 0)[0]
+    max2 = np.amax(set1, axis = 0)[0]
+    LatestDate = min([max1,max2])
+    
+    return(EarliestDate, LatestDate)
+
+
+#This function is removing the date present in Dico1 and not in Dico2 and vice versa
+def removeSomeDate (Dico1,Dico2):
+    for index in Dico1.items():
+        if not Dico2.get(index[0]):
+            Dico1 = Dico1.drop(labels = index[0])
+    for index in Dico2.items():
+        if not Dico1.get(index[0]):
+            Dico2 = Dico2.drop(index[0])
+    return (Dico1, Dico2)
+
+##########################################################################################################################
+
+
+#                                   IV) Tools
+
+
 
 
 
@@ -107,75 +214,9 @@ def RSIsignal(RSIval):
     else:
         return(-1)
     
-    
- 
-    
-#This function formatData() is extracting the data from the excel files and return a dictionnary with the content of the excel sheets
-#dataExcel() allows to read an excel File
-#getFiles () allows to get all excel files in a file
-def formatData():
-    data = []
-    FilesNames = getFiles(my_Path)
-    for i in range(len(FilesNames)):
-        DataArray = dataExcel(FilesNames[i] + '.xlsx')
-        date = DataArray.loc[:,'date']
-        value = DataArray.iloc[:,[1]].values.tolist()
-        TemporaryDic = pd.Series(value)
-        TemporaryDic.index = date
-        data.append(TemporaryDic)
-    DataDic = pd.Series(data)
-    DataDic.index = FilesNames
-    return(DataDic)
-    
-def dataExcel (FileName):
-    data = pd.read_excel (my_Path + FileName)
-    return(data)
-
-
-def getFiles(path):
-    FilesNames = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.xlsx' in file:
-                FilesNames.append(file.replace('.xlsx','',1))
-    return(FilesNames)
-
-
-
-#####                  NOT WORKING
-
-
-#This function is extracting from all the data set the intersection set usable
-# set1 and set2 are supposed to be pd.Series
-def updateData(Dico):
-    
-    #first we need to find the min and the max Date to all the data set
-    commonDate = ['2020-01-01', date du jour]
-    for item1 in Dico.iteritems:
-        for item2 in Dico.iteritems:
-            result = intersectionSet(item1[1], item2[1])
-            if result[0] > commonDate[0]:
-                commonDate[0] = result[0]
-            if result[1] < commonDate[1]:
-                commonDate [1] = result[1]
-    
-    #secondly we have to update every data set accordingly
-    for item1 in Dico.iteritems:
-        for item2 in item1[0].iteritems:
-            if item2[0] < commonDate[0] or item2[0] > commonDate[1]:
-                item1[0].drop(item2[0])
-                
-    return(Dico)   
         
-def intersectionSet (set1, set2):
-    min1 = min(set1.index)
-    min2 = min(set2.index)
-    EarliestDate = max([min1,min2])
-    max1 = max(set1.index)
-    max2 = max(set2.index)
-    LatestDate = min([max1,max2])
-    
-    return(EarliestDate, LatestDate)
 
-print(main())
+
+
+
+#print(main())
